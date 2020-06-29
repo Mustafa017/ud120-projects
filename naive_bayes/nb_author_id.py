018316e54dc3e516f02df2feb50b23cb0fc3d8af
Mustafa017 <mustafangure95@gmail.com>
2020-06-29 09:13:18 -0500

    feat: Train the model and predict on the test data

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,7 +26,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
+t0 = time()
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
+print "training time", round(time()-t0, 3),"s" #1.138 s
+
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time", round(time()-t1, 3),"s" #0.192 s
+
+accuracy = accuracy_score(labels_test, pred)
+print accuracy #0.973265073948
 
 #########################################################
 
